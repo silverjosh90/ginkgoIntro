0504be19de342b1b268470637be82a52ba1a4fd2
localadmin <localadmin@compozed-tss-15.mobile.asu.edu>
2016-07-20 16:19:06 -0700

    Add item remove item working

--- testingIntro.go
+++ testingIntro.go
@@ -1,9 +1,32 @@
 package testingIntro
-
-func main() {
-  firstTestFunc("Josh")
+import (
+  "fmt"
+)
+type ShoppingCart struct {
+   inventory []string
 }
 
-func firstTestFunc(name string) string{
-  return "Hello " + name
+func main() {
+  // firstTestFunc("Josh")
+}
+
+func (SC *ShoppingCart) addItem(item string) {
+  SC.inventory = append(SC.inventory, item)
+}
+
+// func firstTestFunc(name string) string{
+//   return "Hello " + name
+// }
+
+func (SC *ShoppingCart) removeItem(item string) {
+
+  for i:= len(SC.inventory) - 1; i >=0; i-- {
+
+    if(SC.inventory[i] == item) {
+      fmt.Println("THIS IS I", i)
+      SC.inventory = append(SC.inventory[:i], SC.inventory[i+1:]...)
+
+      // SC.inventory = SC.inventory[:i+copy(SC.inventory[i:], SC.inventory[i+1:])]
+      }
+  }
 }
--- testingIntro_test.go
+++ testingIntro_test.go
@@ -4,14 +4,41 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("TestingIntro", func() {
+var _ = Describe("Shopping Cart", func() {
 
-	Context("first func", func(){
-		It("Given the name Josh", func(){
-			Expect(firstTestFunc("Josh")).To(Equal("Hello Josh"))
+	Context("add Item", func(){
+		var(
+			newCart ShoppingCart
+		)
+		It("Given an item, should add it to the shopping cart list", func(){
+			newCart.addItem("potatoes")
+			Expect(newCart.inventory[0]).To(Equal("potatoes"))
 		})
-		It("Given the name Jacob", func(){
-			Expect(firstTestFunc("Jacob")).To(Equal("Hello Jacob"))
+
+		It("Can have multiple items in inventory", func(){
+			newCart.addItem("Spam")
+			Expect(newCart.inventory[1]).To(Equal("Spam"))
 		})
+
+	})
+	Context("remove item", func() {
+		var(
+			newCart ShoppingCart
+		)
+		It("Can remove items", func(){
+			newCart.addItem("potatoes")
+			newCart.addItem("taco")
+			newCart.removeItem("potatoes")
+			Expect(newCart.inventory[0]).To(Equal("taco"))
+		})
+
+		It("can remove from the middle of the array", func(){
+			newCart.addItem("chimi")
+			newCart.addItem("egg")
+			newCart.addItem("borche")
+			newCart.removeItem("borche")
+			Expect(len(newCart.inventory)).To(Equal(3))
+		})
+
 	})
 })
